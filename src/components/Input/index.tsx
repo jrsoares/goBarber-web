f789f059bfcc0066de6cf4401588875edccbba0e
Jr.Soares <juniorsoaresti@gmail.com>
2020-05-04 10:46:15 -0400

    Commit inicial

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,13 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
 
 import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
 import { Container } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -9,11 +16,64 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
-  <Container>
-    {Icon && <Icon size={20} />}
-    <input {...rest} />
-  </Container>
-);
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  /*
+
+    verifica cada input que foram registrados
+    buscou a referencia dos inputs na DOM
+    acessou a propriedade "value"
+    e retornou um campo exatamente com o mesmo nome "fieldName"
+
+  */
+
+  // acessa o elemento na DOM, retornando a referencia do elemento HTML
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  // hook que retorna varias propriedades, registerField - para registrar o campo
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  /*
+    LEI: Sempre que formos criar uma function dentro de um componente (Function),
+    vamos utilizar um hook chamado useCallback (ficam salvas na memoria, não são recriadas toda vez que o componente e renderizado)
+  */
+
+  // focus no input
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    // verifica se o valor do input esta preenchido
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
+  useEffect(() => {
+    // registro dos inputs
+    registerField({
+      name: fieldName,
+      // current retorna o input
+      ref: inputRef.current,
+      // retorn o valor do input
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+  return (
+    <Container isFilled={isFilled} isFocused={isFocused}>
+      {Icon && <Icon size={20} />}
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
+    </Container>
+  );
+};
 
 export default Input;
